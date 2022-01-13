b41be0ab6693c5596ba013a4ed3cbd39c1afbc65
Glokamole <thomasguigard@gmail.com>
2022-01-13 11:24:16 -0500

    :package: update mongodb in memory package

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -3,12 +3,9 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
 
+
 declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(): Promise<string[]>;
-    }
-  }
+  var signin: () => Promise<string[]>;
 }
 
 let mongo: any;
@@ -16,9 +13,8 @@ beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf';
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
-
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
